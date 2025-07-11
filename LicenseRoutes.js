fb7adfc88104d5dd5e5d71348a673f5c59482046
VicMuli <victormuli100@gmail.com>
2025-07-11 15:56:11 +0300

    Add files via upload

--- LicenseRoutes.js
+++ LicenseRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const licenseController = require('../controllers/licenseController');
 
+// POST: Verify license
 router.post('/verify-license', licenseController.verifyLicense);
+
+// GET: Admin panel
 router.get('/admin', licenseController.getAdminPage);
+
+// POST: Add new license
 router.post('/admin/add', licenseController.addLicense);
+
+// GET: Delete license
 router.get('/admin/delete/:id', licenseController.deleteLicense);
 
 module.exports = router;
